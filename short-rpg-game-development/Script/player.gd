48a149554dfc640998ee6e4356c9a6c5f7a8f254
Arnisone <alathtanavong@students.mags.school.nz>
2026-05-25 14:53:21 +1200

    Initial commit

---
+++ short-rpg-game-development/Script/player.gd
@@ -0,0 +1,28 @@
+extends CharacterBody2D
+
+const speed = 100
+
+func _physics_process(delta: float) -> void:
+	pass
+
+func player_movement(delta):
+	player_movement(delta)
+	
+	
+	if Input.is_action_pressed("ui_right"):
+		velocity.x = speed
+		velocity.y = 0
+	elif Input.is_action_pressed("ui_left"):
+		velocity.x = -speed
+		velocity.y = 0
+	elif Input.is_action_pressed("ui_down"):
+		velocity.y = speed
+		velocity.x = 0
+	elif Input.is_action_pressed("ui_ip"):
+		velocity.y = -speed
+		velocity.x = 0
+	else:
+		velocity.x = 0
+		velocity.y = 0
+	
+	move_and_slide()
